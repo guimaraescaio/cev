92c8d529f2b35bf030d5b6ed56c458e51312828c
CaiO Freitas <caiofgmlima@gmail.com>
2022-05-20 07:42:19 -0300

    primeiro commit

---
+++ JAVASCRIPT/array.js
@@ -0,0 +1,5 @@
+var alunos = ["Igor", "José", "Marcos", "Mariana", "Joana", "Leo", "João"];
+
+for (var aluno of alunos){
+    console.log(aluno)
+}
---
+++ JAVASCRIPT/object.js
@@ -0,0 +1,12 @@
+var aluno = {
+    nome: "Igor",
+    notas:[7,8]
+};
+
+var aluno2 = {
+    nome:"José",
+    notas:[5,4]
+};
+
+console.log(aluno);
+console.log(aluno2);
